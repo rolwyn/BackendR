08053ab9e66d8c3939a54325577e508cbfe0ec80
rolwyn <rolwyn.quadras051@gmail.com>
2018-10-11 17:24:23 +0530

    Added app/config.js and changed server.js

---
+++ app/config.js
@@ -0,0 +1,17 @@
+var config = module.exports;
+
+config.db = {
+    username: 'root',
+    password: 'root',
+    name: 'web_project'
+}
+
+config.db.details = {
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql'
+}
+
+config.keys = {
+    secret: '64a831acbb04b5f95447a9f4fdd2c428'
+}
