3983c4de768ca19168b8bb51d1a1b9787dff6855
rolwyn <rolwyn.quadras051@gmail.com>
2018-10-11 17:27:22 +0530

    pushing the remaining changes

--- app/server.js
+++ app/server.js
@@ -1,1 +1,33 @@
-console.log("server is running");
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    morgan = require('morgan'),
+    sequelize = require('sequelize'),
+    passport = require('passport'),
+    jwt = require('jsonwebtoken');
+    mysql = require('mysql');
+
+// Initializations
+var app = express();
+
+// Home Route.
+app.get('/', function(request, response) {
+    response.send("Successfully connected to Server!");
+});
+
+// Starts the Server.
+app.listen('4000', function() {
+    console.log("Listening to Port 4000");
+});
+
+// Parsing the urlencoded and json
+app.use(bodyParser.urlencoded({extended : false})); // If Extended is false, you cannot post Nested Object
+app.use(bodyParser.json());
+
+// Morgan http logger
+app.use(morgan('dev'));
+
+// Setting up Passport
+app.use(passport.initialize()); // Passport is an Authentication middleware for Express
+
+// Static files directory if needed
+// app.use(express.static(__dirname + '../WebProject'));
