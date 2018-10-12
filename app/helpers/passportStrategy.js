c2e65c04bf0dcbaca8c0424f5ec68c248cf7c7cd
rolwyn <rolwyn.quadras051@gmail.com>
2018-10-12 18:23:48 +0530

    Errors are fixed. passportStrategy logic is written

--- app/helpers/passportStrategy.js
+++ app/helpers/passportStrategy.js
@@ -8,18 +8,19 @@ var User = require('./../models/user'),
 function hookJWTStrategy(passport) {
     var options = {};
     options.secretOrKey = config.keys.secret;
-    // options.jwtFromRequest = ExtractJwt.fromAuthHeader();
+    options.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
     options.ignoreExpiration = false;
 
     passport.use(new JWTStrategy(options, function(JWTPayload, callback) {
        User.findOne({ where: {username : JWTPayload.username}}).then(function(user) {
-           if (!user) {
-               callback(null, false);
-               return
+            if (!user) {
+                callback(null, false);
+                return;
             }
-           callback(null, user);
+
+            callback(null, user);
        }) 
-    }))
+    }));
 }
 
 module.exports = hookJWTStrategy;
